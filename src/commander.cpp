f82ff72e9017fc1925b3f16105e1c630f11fda7e
songqian2002 <songqian2002@gmail.com>
2018-01-23 20:58:26 -0500

    added planning stuff

--- src/commander.cpp
+++ src/commander.cpp
@@ -4,6 +4,8 @@
 #include <tf/transform_datatypes.h>
 #include <angles/angles.h>
 
+#include <nav_msgs/GetPlan.h>
+
 using namespace move_base_msgs;
 
 struct Pose {
@@ -35,18 +37,22 @@ void feedbackCb(const MoveBaseFeedbackConstPtr& feedback)
     ROS_INFO_STREAM("Got some feedback. Too lazy to print them");
 }
 
+
+
 int main(int argc, char **argv) {
 
     ros::init(argc, argv, "commander");
     MoveBaseClient ac("move_base", true);
 
-    ROS_INFO("Waiting for move_base to start.");
-    ac.waitForServer();
+    while(!ac.waitForServer(ros::Duration(5.0))) {
+        ROS_INFO("Waiting for the move_base action server to come up");
+     }
+
     ROS_INFO("move_base started, sending goal.");
 
     MoveBaseGoal goal;
     geometry_msgs::Quaternion goal_quaternion = tf::createQuaternionMsgFromYaw(angles::from_degrees(GOAL.angle));
-    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.frame_id = "map";
     goal.target_pose.header.stamp = ros::Time::now();
     goal.target_pose.pose.position.x = GOAL.x;
     goal.target_pose.pose.position.y = GOAL.y;
@@ -55,12 +61,45 @@ int main(int argc, char **argv) {
     goal.target_pose.pose.orientation.z = goal_quaternion.z;
     goal.target_pose.pose.orientation.w = goal_quaternion.w;
 
+    // Chris' service 
+    ros::NodeHandle n;
+    ros::ServiceClient client = n.serviceClient <nav_msgs::GetPlan> ("/move_base/make_plan");
+    nav_msgs::GetPlan srv;
+
+    ros::Publisher plan_pub = n.advertise<nav_msgs::Path>("plan", 1000);
+
+    // GetPlan -> start
+    srv.request.start.pose.position.x = 0; // start -> PoseStamped 
+    srv.request.start.pose.position.y = 0;
+    srv.request.start.pose.position.z = 0;
+    srv.request.start.pose.orientation.x = 0; 
+    srv.request.start.pose.orientation.y = 0;
+    srv.request.start.pose.orientation.z = 0;
+    srv.request.start.pose.orientation.w = 0;
+    srv.request.start.header.frame_id = "map";
+    srv.request.start.header.stamp = ros::Time::now();
+    
+    // GetPlan -> goal
+    srv.request.goal = goal.target_pose; // goal -> PoseStamped
+
+    // GetPlan -> tolerance
+    srv.request.tolerance = 0.5;
+
+
+    if (client.call(srv)) {
+        plan_pub.publish(srv.response.plan);
+    }
+    else {
+        ROS_ERROR("Failed to call service");
+        return 1;
+    }
+
     ac.sendGoal(goal);
 
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    /*if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
         ROS_INFO("Hooray, the base moved 1 meter forward");
     else
         ROS_INFO("The base failed to move forward 1 meter for some reason");
-
+*/
     return 0;
 }
